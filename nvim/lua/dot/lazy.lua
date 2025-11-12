295c2be2fe0dded30b5b8c37936750818f5b133f
David Harris <dhharris9@gmail.com>
2025-11-12 13:01:24 +0100

    lazy: disable change notification

--- nvim/lua/dot/lazy.lua
+++ nvim/lua/dot/lazy.lua
@@ -22,4 +22,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("dot.plugins")
+require("lazy").setup("dot.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
